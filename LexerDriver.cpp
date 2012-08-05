d4edb9af9bb9877c3925e53650ec2d41bd1eb621
Omar <oahsan@umd.edu>
2012-08-04 20:34:58 -0400

    Lexer 0.12
    Driver now reads the proper length of the file.

--- LexerDriver.cpp
+++ LexerDriver.cpp
@@ -7,7 +7,7 @@ int main() {
   string file;
   getline(cin, file); 
   int pos = 0;
-  while (pos < file.length()) {
+  while (pos <= file.length()) {
     cout << getToken(file, &pos) << endl;
   }
   return 0;
