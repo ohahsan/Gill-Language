9a3676c21159479658b0a5bbf35c59a7f0c729a2
Omar Hossain Ahsan <oahsan@umd.edu>
2013-04-04 20:43:21 -0400

    Began the parser with the GUIF statement.

--- Parser.cpp
+++ Parser.cpp
@@ -3,16 +3,68 @@
 #include <string>
 #include <vector>
 #include "Parser.h"
+#include "Compiler.h"
 
 using namespace std;
 
-extern string word;
-extern int number;
+static void parseGuif(vector<TokenClass> tokens);
+static void evalGuif(vector<TokenClass> conditions
+		     , vector<TokenClass> trues);
+
+unsigned int position = 0;
 
 /* parse -
 
    Uses the lexer to generate tokens and then parses them. */
 
+
 void parse(vector<TokenClass> tokens) {
-  
+  for (position = 0; position < tokens.size(); position++) {
+    TokenClass curTok = tokens[position];
+    int token = curTok.tok;
+    // string id = curTok.id;
+    // int num = curTok.num;
+    
+    if (token == GUIF) {
+      parseGuif(tokens);
+    }
+
+  }
+}
+
+/* parseGuif -
+
+   Organizes the tokens into the condition and the
+   statements to evaluate. */
+
+void parseGuif(vector<TokenClass> tokens) {
+  cout << "Parsing an if!" << endl;
+  vector<TokenClass> statement;
+  vector<TokenClass> trues;
+  position++;
+  position++; // Ignore the '('
+  while (tokens[position].tok != ')') {
+    statement.push_back(tokens[position]);
+    position++;
+  }
+  position++; // Ignore the ')'
+  position++; // Ignore the '{'
+  while (tokens[position].tok != '}') {
+    trues.push_back(tokens[position]);
+    position++;
+  }
+  position++; // Ignore the '}'
+  evalGuif(statement, trues);
+}
+
+/* evalGuif -
+
+   Evaluates the guif statement. 
+   NOT started... */
+
+void evalGuif(vector<TokenClass> condition, vector<TokenClass> trues) {
+  cout << "Condition: " << endl;
+  printTokens(condition);
+  cout << "If true: " << endl;
+  printTokens(trues);
 }
